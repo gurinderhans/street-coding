23a37e7e85b156b0933a18e98feed5b4f7808bee
Gurinder Hans <hello@gurinderhans.me>
2018-08-30 19:24:57 -0700

    add twosum

--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -1,6 +1,17 @@
-inp_arr = [2, 45, 12, 31, 82]
-target = 76
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashtbl = {}
+        for i in range(len(nums)):
+          if nums[i] in hashtbl:
+            return [hashtbl[nums[i]], i]
+          else:
+            hashtbl[target - nums[i]] = i
 
-def find_nums(inp_arr, target):
-  for num in inp_arr:
-       
+
+res=Solution().twoSum([2,7,11,15], 9)
+print(res)
